91d829c7a03dcffca19cf93cce8f1ae315eae28c
Swizaesar Reza Ryann <swizaesarrezaryann@Swizaesars-MacBook-Air.local>
2023-05-23 00:12:17 +0700

    fix: fix refer in archive dahsbord

--- src/components/Utils/RandomColor.js
+++ src/components/Utils/RandomColor.js
@@ -2,7 +2,6 @@ const randomColor = (id) => {
     const randomColourOne = '#50a1d8'.replace(/0/g, function () {
         return (~~(id * 16)).toString(16);
     });
-    console.log(id);
     var colors = [randomColourOne];
     var random_color = colors[Math.floor(Math.random() * colors.length)];
     return random_color.length < 5 ? random_color + 'a' : random_color;
