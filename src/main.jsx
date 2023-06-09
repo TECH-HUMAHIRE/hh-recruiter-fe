adf380981a4ed1dd09c467ca3696aac6ef6bcd50
Swizaesar Reza Ryann <swizaesarrezaryann@Swizaesars-MacBook-Air.local>
2023-06-09 17:06:53 +0700

    fix : vercel config header for service worker 2

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { store } from './app/store';
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/service-worker.js')
+            .register('../service-worker.js')
             .then((registration) => {
                 console.log('Service worker registered:', registration);
             })
