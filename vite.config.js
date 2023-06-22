18c6789643b59c5e90537bc9c24a82fc9549c41a
Yoga Trista <yoga.trista@fithub.id>
2023-06-23 06:15:53 +0700

    change url

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode }) => {
             esbuildOptions: {
                 plugins: [esbuildCommonjs(['midtrans-client'])]
             }
-        }
+        },
         base: '/'
     };
 });
