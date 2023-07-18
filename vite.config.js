2ed74491d013d66d0fa0647c7b2a30e1ae142305
unknown <yoga.trista@fithub.id>
2023-07-18 08:58:21 +0700

    typo

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,6 @@ export default defineConfig(({ mode }) => {
             esbuildOptions: {
                 plugins: [esbuildCommonjs(['midtrans-client'])]
             }
-        },
+        }
     };
 });
